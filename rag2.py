ceb0267d5f2b925cd082ed7985a5899db8e3fc98
Chak Hang Chan <102423197+maxchanhi@users.noreply.github.com>
2024-05-21 20:34:14 +0800

    Update rag2.py

--- rag2.py
+++ rag2.py
@@ -4,41 +4,24 @@ import streamlit as st
 from streamlit_modal import Modal
 
 def rag_feedback(student_result):
-    import os
-    from langchain_community.document_loaders import TextLoader
-    from langchain.text_splitter import RecursiveCharacterTextSplitter
-    from langchain_community.embeddings import OpenAIEmbeddings
+    from streamlit_modal import Modal
     from langchain_community.vectorstores import FAISS
     from langchain.prompts import ChatPromptTemplate
     from langchain_community.chat_models import ChatOpenAI
-    DOC_PATH = "knowledge.txt"
 
-# Set up OpenAI API key
+    INDEX_PATH = "faiss_index"
+
     OPENAI_API_KEY = st.secrets["OpenAI_key"]
 
-# ----- Data Indexing Process -----
+    # Load the precomputed FAISS index from disk
+    db_faiss = FAISS.read_index(INDEX_PATH)
 
-# load your text file
-    loader = TextLoader(DOC_PATH)
-    pages = loader.load()
-
-    # split the doc into smaller chunks i.e. chunk_size=500
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=20)
-    chunks = text_splitter.split_documents(pages)
-
-    # get OpenAI Embedding model
-    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
-
-    # embed the chunks as vectors and load them into the database
-    db_faiss = FAISS.from_documents(chunks, embeddings)
-
-    # retrieve context - top 5 most relevant (closest) chunks to the query vector
     docs_faiss = db_faiss.similarity_search(student_result, k=5)
 
-    # generate an answer based on given user query and retrieved context information
+    # Generate an answer based on given user query and retrieved context information
     context_text = "\n\n".join([doc.page_content for doc in docs_faiss])
 
-    # load retrieved context and user query in the prompt template
+    # Load retrieved context and user query in the prompt template
     PROMPT_TEMPLATE = """
     You are a music theory teacher. Please provide feedback or answer questions about music theory based on the given context:
     {context}
@@ -50,7 +33,7 @@ def rag_feedback(student_result):
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     prompt = prompt_template.format(context=context_text, student_result=student_result)
 
-    # call LLM model to generate the feedback based on the given context and student result
+    # Call LLM model to generate the feedback based on the given context and student result
     model = ChatOpenAI(openai_api_key=OPENAI_API_KEY)
     feedback = model.predict(prompt)
     return feedback
